705499576a6a1f6a62b8a62e3a495dd45292d802
Ambrose Hundal <amshunda@ucsc.edu>
2018-02-04 14:45:29 -0800

    update insert function

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,1 +1,73 @@
 
+#ifndef LinkedList.h
+#define LinkedList.h
+
+
+
+template <class T>
+struct Node {
+  T data;
+  Node <T>* pointer;
+  
+}
+
+
+
+
+class LinkedList{
+private:
+Node <T>* head;     //pointer to first node
+int count;          // no of nodes in the list
+
+
+
+public:
+  //constructor & destructor
+  LinkedList();
+  ~LinkedList();
+  
+  
+  //functions
+ virtual void newlist(T node);  
+ 
+  
+  
+  //add an item to list
+ void addAnywhere(<T> x, int n){
+      //create new node and initialize
+      Node<T>* temp1 = new Node<T>();
+      temp1->data=data;
+      temp1->pointer=null; 
+        
+      
+      if(n==1){
+        temp1->pointer = head;
+        head = temp1;
+        return;
+      }
+        
+      Node<T>* temp2 = head;
+       for(int i = 1; i<n; i++){
+         temp2=temp2->pointer;
+       }
+         temp1->pointer=temp2->pointer;
+         temp2->pointer=temp1;   
+ } ;
+  
+  
+ virtual void deleteNode();
+ virtual void findNode();
+  
+  //get the total items in list
+  template <class T>
+  int LinkedList<T> :: countItems(){
+    return count;
+    
+  }
+      
+    ;
+  bool EmptyList();
+  bool isEmpty();
+  
+  
+  
