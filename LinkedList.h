869b150625d1d1a314113c16e87afc346e019045
Ambrose Hundal <amshunda@ucsc.edu>
2018-02-03 16:05:49 -0800

    Update LinkedList.h

--- LinkedList.h
+++ LinkedList.h
@@ -1,1 +1,26 @@
+#ifndef LinkedList.h
+#define LinkedList.h
 
+
+
+template <class ListType>
+class LinkedList{
+
+//data attributes
+//node
+
+public:
+  //constructor & destructor
+  LinkedList();
+  ~LinkedList();
+  
+  
+  //functions
+  Node<ListType>* newlist();        
+  <ListType> addNode();
+  <ListType> deleteNode();
+  <ListType> findNode();
+  <ListType> countItems();
+  <ListType> EmptyList();
+  
+  
