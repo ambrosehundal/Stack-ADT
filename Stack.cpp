11023719e12ab6a765819442303bb4edb6771811
Ambrose Hundal <amshunda@ucsc.edu>
2018-02-05 18:53:44 -0800

    implement functions Stack ADT

--- Stack.cpp
+++ Stack.cpp
@@ -1,1 +1,52 @@
 #include stack.h
+
+
+
+//constructor
+template <class S>
+class Stack:: public LinkedList <T>       //Stack inherits singly linkedlist
+stack<S>:: stack(){
+  top = nullptr;
+  stacksize = 0;
+}
+
+
+//push
+template <class S>
+void stack<S>:: push(S s){
+  if(isFull()){
+    cout << "Stack is full";
+    exit(EXIT_FAILURE);
+  }
+  [++top] = s;
+  
+  
+
+
+//pop
+template <class S>
+  S stack<S>:: pop(){
+    
+    if(isEmpty()){
+      cout << "Empty stack" ;
+      exit(EXIT_FAILURE);
+    }
+    return [top]--;
+    
+//isEmpty
+  template <class S>
+    bool stack<S>:: isEmpty(int stacksize){
+      return(stacksize == 0);
+    }
+    
+
+//isFull
+    template <class S>
+      bool stack<S>:: isFull(){
+        return(top == -1);
+      }
+    
+
+
+
+
