8ef59bdc7cd648e5a11787b16e4c20edecc846bb
Ambrose Hundal <amshunda@ucsc.edu>
2018-03-01 21:44:03 -0800

    Update LinkedList.h
    need to define the remaining functions

--- LinkedList.h
+++ LinkedList.h
@@ -1,37 +1,111 @@
-#ifndef LinkedList.h
-#define LinkedList.h
+//Ambrose Hundal
 
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
+
+#include "Node.h"
 
 
 template <class T>
-struct Node {
-  T data;
-  Node <T>* pointer;
-  
+class LinkedList : public Node
+
+private:
+	Node<T> headptr;
+	int itemcount;
+
+public:
+	LinkedList();
+	LinkedList(const Node<T>* head);
+	~LinkedList();
+
+//do you need accessor and mutator methods?
+
+/*functions*/
+//void newlist()
+
+void insertNode();
+void deleteNode();
+bool findNode();
+int getsize();
+void clear();
+
+
+/*******Implementation************/
+
+
+/******************************************
+* LinkedList()
+* 
+*/
+template <class T>
+LinkedList<T>::LinkedList(){
+
+	headptr = nullptr;
+	itemcount = 0;
+}
+
+/******************************************
+* LinkedList()
+* parametrized constructor
+*/
+template <class T>
+LinkedList<T>::LinkedList(const Node<T>* head){
+
+	headptr = nullptr;
+	itemcount = 0;
+}
+
+/******************************************
+* void insertNode();
+* adds a node entry to the linkedlist at the specified index
+*/
+template <class T>
+void LinkedList<T>::insertNode(T nodeval){
+	                                        //double check the parameters based on the stack implementation
 }
 
 
 
+/******************************************
+* void deleteNode();
+* deletes a node entry 
+*/
+template <class T>
+void LinkedList<T>::deleteNode(T nodeval){
+	                                       
+}
 
-class LinkedList{
-private:
-Node <T>* head;     //pointer to first node
-int count;          // no of nodes in the list
+
+
+/******************************************
+* bool findNode();
+* returns true if the node specified in the parameter is found
+*/
+template <class T>
+bool LinkedList<T>::findNode(T nodeval){
+	                                       
+}
+
+
+/******************************************
+* int getsize()
+* returns the number of items in the list
+*/
+template <class T>
+int LinkedList<T>::getSize(){
+	return itemcount;
+}
 
 
 
-public:
-  //constructor & destructor
-  LinkedList();
-  ~LinkedList();
-  
-  
-  //functions
-  void newlist(T node);        
-  void addNode(T node);
-  void deleteNode();
-  void findNode();
-  int countItems();
-  bool EmptyList();
-  
-  
+/******************************************
+* void clear();
+* clears the list
+*/
+template <class T>
+void LinkedList<T>::clear(){
+	headptr = null;
+	itemcount = 0;
+}
+
+#endif
